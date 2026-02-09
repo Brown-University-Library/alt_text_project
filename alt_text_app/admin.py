53aef951f2862cc3a898299c32a42b38ad912bb7
Birkin James Diana <birkin.diana@gmail.com>
2026-02-09 17:34:46 -0500

    adds image-preview to admin.

--- alt_text_app/admin.py
+++ alt_text_app/admin.py
@@ -1,4 +1,7 @@
+import base64
+
 from django.contrib import admin
+from django.utils.html import format_html
 
 from alt_text_app.models import ImageDocument, OpenRouterAltText
 
@@ -8,6 +11,20 @@ class ImageDocumentAdmin(admin.ModelAdmin):
     """
     Admin interface for ImageDocument model.
     """
+
+    def thumbnail_preview(self, obj: ImageDocument) -> str:
+        """
+        Renders the thumbnail preview image in admin.
+        """
+        if not obj.thumbnail_webp:
+            return 'Missing thumbnail'
+        encoded_thumbnail = base64.b64encode(obj.thumbnail_webp).decode('ascii')
+        return format_html(
+            '<img src="data:image/webp;base64,{}" style="max-width: 200px; max-height: 100px;" />',
+            encoded_thumbnail,
+        )
+
+    thumbnail_preview.short_description = 'Thumbnail preview'
 
     list_display = [
         'original_filename',
@@ -31,9 +48,26 @@ class ImageDocumentAdmin(admin.ModelAdmin):
         'file_checksum',
         'file_size',
         'uploaded_at',
+        'thumbnail_preview',
+        'thumbnail_created_at',
+        'thumbnail_width_px',
+        'thumbnail_height_px',
+        'thumbnail_error',
     ]
     fieldsets = [
         ('File Information', {'fields': ['original_filename', 'file_checksum', 'file_size']}),
+        (
+            'Thumbnail',
+            {
+                'fields': [
+                    'thumbnail_preview',
+                    'thumbnail_created_at',
+                    'thumbnail_width_px',
+                    'thumbnail_height_px',
+                    'thumbnail_error',
+                ]
+            },
+        ),
         ('User Information', {'fields': ['user_first_name', 'user_last_name', 'user_email', 'user_groups']}),
         ('Status', {'fields': ['processing_status', 'processing_error', 'uploaded_at']}),
     ]
